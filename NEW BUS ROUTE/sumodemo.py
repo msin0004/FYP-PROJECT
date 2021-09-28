cdc3d4a4c6abfd9712002fe3553e1a3c0f1f367d
msin0004 <84778025+msin0004@users.noreply.github.com>
2021-09-28 19:56:04 +1000

    Updating server code
    Adding code to send data from the main function to the server thread. This is achieved with a manager. Currently strings of data can be sent  but numbers have issues. This will need to be addressed with the next commit

--- NEW BUS ROUTE/sumodemo.py
+++ NEW BUS ROUTE/sumodemo.py
@@ -9,13 +9,16 @@ import os, sys
 import traci
 import multiprocessing
 from multiprocessing import Process, Value, Manager
+from ctypes import c_wchar_p
 import socket
 import time
 import threading
+stop = 'stop0'
+sending_data = "sample"
 
 
 # server program:  creates the server, handles incoming calls and subsequent user requests
-def server(data,central_authority_broadcast):
+def server(data,temp,central_authority_broadcast):
 	# size of buffer and backlog
 	buffer = 2048 # value should be a relatively small power of 2, e.g. 4096
 	backlog = 1 # tells the operating system to keep a backlog of 1 connection; this means that you can have at most 1 client waiting while the server is handling the current client; the operating system will typically allow a maximum of 5 waiting connections; to cope with this, busy servers need to generate a new thread to handle each incoming connection so that it can quickly serve the queue of waiting clients
@@ -33,25 +36,78 @@ def server(data,central_authority_broadcast):
 	realVehicleIndex = 0
 	#central_authority_broadcast[realVehicleIndex] = 'hello before loops'
 
+	 # bus stop that the user is at
+
 	def clientHandle(client_socket, address, realVehicleIndex):
 
 		while True: # infinite loop 2
 			incoming = client_socket.recv(buffer).decode('UTF-8') # receive client data into buffer
 			print("newthread loop")
 			print(incoming)
+			print(data.value)
 			if (incoming == 'quit'):
 				print("Ending session with client.")
 				client_socket.close() # close the connection with the client
 				central_authority_broadcast[realVehicleIndex] = 'No Advice'
 				break # breaks out of infinite loop 2
 			if (incoming == 'update'):
-				print("sending Data")
+				#print("sending Data")
 				#central_authority_broadcast[realVehicleIndex] = "hello"
 				#client_socket.send(central_authority_broadcast[realVehicleIndex] + '\n') # send the data to the client
-				message = "hallo \n"
+				message = "test message \n"
+				#central_authority_broadcast[0] = 'testing'
+				print(central_authority_broadcast[0])
 				client_socket.send(message.encode())
-
+			if (incoming == 'get'):
+				print("sending get data")
+				message = "get data being sent \n"
+				client_socket.send(message.encode())
 				#print(central_authority_broadcast[realVehicleIndex] + '\n')
+			if (incoming == 'send'):
+				global sending_data
+				print("sending data print in incoming\n")
+				print(temp.value)
+				print("sending data print after incoming\n")
+				message = str(temp.value)
+				print("darta val")
+				print(data.value)
+				print("data val end")
+				print("message being sent")
+				print(message)
+				print("message being sent end")
+				client_socket.send(message.encode())
+				print("data sent")
+
+			if (incoming == 'stop7'):
+				stop = 'stop7'
+				#data = "capacity"
+				data.value = 1
+				print("data")
+				print(data.value)
+#				while data == 'capacity':
+	#				temp = 0
+
+
+				#message = 'testing\n'
+				#client_socket.send(mesasge.encode())
+
+			if (incoming == 'stop1'):
+				stop = 'stop1'
+			if (incoming == 'stop2'):
+				stop = 'stop2'
+			if (incoming == 'stop3'):
+				stop = 'stop3'
+			if (incoming == 'stop4'):
+				stop = 'stop4'
+			if (incoming == 'stop5'):
+				stop = 'stop5'
+			if (incoming == 'stop6'):
+				stop = 'stop6'
+			if (incoming == 'stop8'):
+				stop = 'stop8'
+			if not incoming:
+				stop = 'no incoming'
+				print('no incoming')
 
 	while True: # infinite loop 1
 		client_socket, address = server_socket.accept() # passively accept TCP client connections; the call returns a pair of arguments: client is a new Socket object used to communicate with the client and address is the address of the client
@@ -77,10 +133,12 @@ if __name__ == '__main__':
 	# initialisations
 	step = 0 # time step
 	d = Value('d', 0.0) # 'd' is a string containing a type code as used by the array module (where 'd' is a floating point number implemented in double in C) and 0.0 is an initial value for 'd'
+	#d = 'temp'
 
 	manager = Manager()
 	central_authority_broadcast = manager.list()
-	thread = Process(target=server, args=(d,central_authority_broadcast)) # represents a task (i.e. the server program) running in a subprocess
+	temp = manager.Value(c_wchar_p, "")
+	thread = Process(target=server, args=(d, temp, central_authority_broadcast)) # represents a task (i.e. the server program) running in a subprocess
 
 
 	print()
@@ -116,7 +174,7 @@ if __name__ == '__main__':
 	capacity = 1				#bus Capacity
 	#next_stop = "x" 			#next stop for the bus (CREATED IN THE LOOP BELOW)
 	bus_id = 'bus_flow.0'		#temporary variable to store bus id for data collection (need to change to allow dynamic readings)
-
+	t = 0
 
 	while step < endSim:
 		thread.join(timeout) # implicitly controls the speed of the simulation; blocks the main program either until the server program terminates (if no timeout is defined) or until the timeout occurs
@@ -125,13 +183,17 @@ if __name__ == '__main__':
 		#print('Current value of d: {}'.format(d.value))
 		#print(traci.simulation.getLoadedIDList())
 		if step > 3000:
+			print(temp.value)
+			#central_authority_broadcast.append = 'test'
+			#print(central_authority_broadcast[0])
+
 			#bus_eta(bus_id)
 		#if step%10000 == 0 and step > 1000:
 			#this shows a list of vehicle ids
 			#bus_id_all = print(traci.vehicle.getIDList())
 
 			# bus occupancy
-			#occupied = traci.vehicle.getPersonNumber(bus_id)
+			occupied = traci.vehicle.getPersonNumber(bus_id)
 			#print('Person in vehicle 1: ' + str(occupied))
 
 			# the following code provides the next bus stop the bus is going to
@@ -170,9 +232,39 @@ if __name__ == '__main__':
 			#print('Capacity of vehicle 1: ' + str(capacity))
 
 			#inputs from app
+			#print(d)
 			if(d.value == 2):
 				print("INPUT FROM APP")
-				d.value = 0
+				#d = 0
+			#if(d == 'capacity'):
+			#	bus_id = 'bus_flow.0'
+			#	print("bus_flow_0")
+			#	d = traci.vehicle.getPersonCapacity(bus_id)
+			#	print(d)
+			#print(d.value)
+			if(d.value == 1):
+				print("sending data printing")
+				print(sending_data)
+				print("sending data printed now message")
+				message = "hello \n"
+				sending_data = message
+				if t == 0:
+					temp.value = message
+					t = t+1
+				elif t == 1:
+					message = "nononono\n"
+					temp.value = message
+					t = t+1
+				elif t == 2:
+					message = "1"
+					temp.value = str(message)
+					t = 0
+				print()
+				#temp.value = occupied
+				print(occupied)
+				print(temp.value)
+				print("sending data printed change")
+				d.value = 9
 
 		# go to the next time step
 		step += 1000 # in milliseconds
